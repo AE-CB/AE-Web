5bdb0a34ed4932b80392a6acac164514efe04570
vimuth <vimuths@yahoo.com>
2022-11-18 23:29:35 +0530

    Fix hidration errors

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Header from '../components/Header.tsx'
 import Footer from '../components/Footer.tsx'
+import Script from 'next/script'
 
 export default function Document() {
     return (
@@ -10,7 +11,7 @@ export default function Document() {
                 <title> CarHouse - Inicio </title>
                 <link rel="shortcut icon" href="./assets/img/logo/icon-shortcut.png" />
                 {/* Fontawesome */}
-                <script src="https://kit.fontawesome.com/de0eb60c48.js" crossOrigin="anonymous"></script>
+                <Script src="https://kit.fontawesome.com/de0eb60c48.js" crossOrigin="anonymous"></Script>
                 {/* AOS (Animate On Scroll) */}
                 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
 
