cf2bbec1fd9a8cd9e45e2691f60d6027097d75c0
vimuth <boomer@boomerbaker.com>
2023-01-02 16:43:44 +0530

    Add AOS

--- pages/_document.js
+++ pages/_document.js
@@ -9,26 +9,19 @@ import { useRouter } from "next/router";
 export default function Document() {
     return (
         <Html lang='es'>
-            <Head />
-            <head>
+            <Head>
                 <title> CarHouse - Inicio </title>
                 <link rel="shortcut icon" href="./assets/img/logo/icon-shortcut.png" />
                 {/* Fontawesome */}
                 <Script src="https://kit.fontawesome.com/de0eb60c48.js" crossOrigin="anonymous"></Script>
-                {/* AOS (Animate On Scroll) */}
-                <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
 
-            </head>
+            </Head>
             <body>
                 {/* <Header /> */}
                 <Main />
                 <NextScript />
                 
                 {/* <Footer/> */}
-                <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-                {/* <script>
-                    AOS.init();
-                </script> */}
             </body>
 
         </Html>
